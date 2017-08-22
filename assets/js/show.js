e60fc3461872ba079c9c6e66c64319386245d262
yinghua <956424435@qq.com>
2017-08-23 00:56:25 +0800

    循环完成8.22

---
+++ assets/js/show.js
@@ -0,0 +1,21 @@
+$(function() {
+    var content = '';
+    console.log(1111);
+    /* $.get('../php/1.php', function(msg) {
+             //var content = '';
+             msg.forEach(function(value, key) {
+                 content += `<li class="list">
+                        <img src="${value.img}" alt="">
+                         <h3><a href="#">${value.title}</a></h3>
+                         <h4>
+                       	<i class="iconfont icon-yanjin"></i>114
+                        <i class="iconfont icon-xixin"></i>12
+                        <span>${value.time}</span>		
+                        </h4>
+                       </li>`;
+             })
+
+             console.log(content);
+         }, 'json')
+         //$('.list').html(content);*/
+})
---
+++ assets/php/1.php
@@ -0,0 +1,22 @@
+<?php
+echo'[
+{"img":"assets/imagesshow/tj1.jpg","title":"Dango 扶手椅 / Agieszka Kowal","time":"阿奇马佳.12小时前"},
+
+{"img":"assets/imagesshow/tj2.jpg","title":"Szabelski 巧克力包装设计","time":"阿奇马佳.6小时前"},
+
+
+{"img":"assets/imagesshow/tj3.jpg","加拿大红十字会（CRC）重塑品牌形象，提示识别度","time":"阿奇马佳.1天前"},
+
+{"img":"assets/imagesshow/tj4.jpg","title":"苹果iOS11 不仅改了界面，还重新设计了这9个应用图标","time":"阿奇马佳.2天前"},
+
+{"img":"assets/imagesshow/tj5.jpg","title":网椅/众产品,"time":"阿奇马佳.6小时前"},
+
+{"img":"assets/imagesshow/tj6.jpg","title":"25个美丽的珠宝包装设计","time":"阿奇马佳.3天前"},
+
+
+
+{"img":"assets/imagesshow/tj7.jpg","title":"Oh Miel 蜂蜜包装设计","time":"阿奇马佳.6小时前"},
+
+{"img":"assets/imagesshow/tj8.jpg","title":"2018狗年全球吉庆生肖设计大赛","time":"阿奇马佳.6天前"}
+
+]';
