d8df3048c4ff67fe12bbb88783afacdde1f45739
yinghua <956424435@qq.com>
2017-08-25 19:10:47 +0800

    中心页

--- assets/js/center.js
+++ assets/js/center.js
@@ -1,1 +1,13 @@
+$(function() {
+    $('.kuang').click(function() {
+        // if ()
 
+        $('.xiala').removeClass('icon-xia');
+        $('.xiala').addClass('icon-shang');
+        $('.xuanx').toggle();
+
+    })
+
+
+
+})
